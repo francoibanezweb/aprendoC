98a0fb9d03a78d310859aec0a1b3a91d9209a362
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-06 18:07:23 -0300

    resolves ej_02 and ej_03

--- src/01_dialogo_con_el_usuario/ej_02.c
+++ src/01_dialogo_con_el_usuario/ej_02.c
@@ -1,6 +1,12 @@
+/***************
+| Ap.01, Ej.02 |
+***************/
+
 #include <stdio.h>
 
 int main (){
-    printf("Hola, soy ej_02!");
+    int numEjemp;           /* Impresión de un entero */
+    numEjemp = 9;
+    printf("El número del ejemplo es %d. \n",numEjemp);
     return 0;
 }
--- src/01_dialogo_con_el_usuario/ej_03.c
+++ src/01_dialogo_con_el_usuario/ej_03.c
@@ -1,6 +1,11 @@
+/***************
+| Ap.01, Ej.03 |
+***************/
 #include <stdio.h>
 
 int main (){
-    printf("Hola, soy ej_03!");
+    char charEjemp;              /* Impresión de un carácter */
+    charEjemp = 'a';
+    printf("El carácter del ejemplo es %c. %n", charEjemp);
     return 0;
 }
