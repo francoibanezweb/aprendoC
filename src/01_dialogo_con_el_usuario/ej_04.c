b10a9adbc3dd1909c3f5febf7f670b0b3a84f39f
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-06 18:11:25 -0300

    resolves ej_04.c

--- src/01_dialogo_con_el_usuario/ej_04.c
+++ src/01_dialogo_con_el_usuario/ej_04.c
@@ -1,6 +1,13 @@
+/***************
+| Ap.01, Ej.02 |
+***************/
 #include <stdio.h>
 
 int main (){
-    printf("Hola, soy ej_04!");
+    char carEjemp;              /* impresión de un carácter */
+    int numEjemp;               /* y un entero. */
+    carEjemp = 'a';
+    numEjemp = 9;                                
+    printf("Los elementos del ejemplo son %c y %d. \n",carEjemp,numEjemp);
     return 0;
 }
