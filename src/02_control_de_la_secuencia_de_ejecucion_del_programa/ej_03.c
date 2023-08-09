2efef93a5a5a99b19aa12de0446a384dc80875df
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-08 22:10:10 -0300

    resolves ej 3

--- src/02_control_de_la_secuencia_de_ejecucion_del_programa/ej_03.c
+++ src/02_control_de_la_secuencia_de_ejecucion_del_programa/ej_03.c
@@ -1,6 +1,28 @@
+/***************
+| Ap.02, Ej.03 |
+***************/
 #include <stdio.h>
 
-int main(){
-  printf("Hola, soy 02_03\n");
+int main()
+{
+  /* versión 3: impresión del código ASCII */
+  /* del carácter tecleado por el usuario */
+  /* con posibilidad de repetir el proceso */
+
+  char carEjemp, repetir;
+  repetir = 'S';
+  while (repetir == 'S' || repetir == 's')
+  {
+    printf("\nCarácter del que desea conocer el código ASCII > ");
+    scanf("\n%c", &carEjemp);
+    printf("El código ASCII de %c es %d", carEjemp, carEjemp);
+    repetir = 'x';
+    while (repetir != 'S' && repetir != 's' && repetir != 'N' && repetir != 'n')
+    {
+      printf("\nDesea repetir el proceso (S/N) > ");
+      scanf("\n%c", &repetir);
+    }
+  }
+
   return 0;
 }
