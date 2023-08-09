01b2fc21d45cf04722708a1bae30d7a9014005ca
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-08 21:33:57 -0300

    resolves ap 02 ej 01

--- src/02_control_de_la_secuencia_de_ejecucion_del_programa/ej_01.c
+++ src/02_control_de_la_secuencia_de_ejecucion_del_programa/ej_01.c
@@ -3,7 +3,23 @@
 ***************/
 #include <stdio.h>
 
-int main(){
-  printf("Hola, soy 02_01\n");
+int main()
+{ /* Impresión del código ASCII del */
+  /* carácter tecleado por el usuario */
+  /* con posibilidad de repetir el proceso */
+
+  char carEjemp, repetir;
+  repetir = 'R';
+  while (repetir == 'R')
+  {
+    printf("\nCarácter del que desea conocer el código ASCII > ");
+    scanf("\n%c", &carEjemp);
+    printf("El código ASCII de %c es %d", carEjemp, carEjemp);
+
+    printf("\n\nPulse la tecla R para repetir el proceso,");
+    printf("\nu otra tecla cualquiera para abandonar el programa ");
+    scanf("\n%c", &repetir);
+  }
+
   return 0;
 }
