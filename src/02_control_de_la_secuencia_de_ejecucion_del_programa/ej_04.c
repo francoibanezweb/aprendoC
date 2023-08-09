43807710e53cc82c5ab6d1d903b9f4f39e15f429
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-08 22:10:17 -0300

    resolves ej 4

--- src/02_control_de_la_secuencia_de_ejecucion_del_programa/ej_04.c
+++ src/02_control_de_la_secuencia_de_ejecucion_del_programa/ej_04.c
@@ -1,6 +1,44 @@
+/***************
+| Ap.02, Ej.04 |
+***************/
 #include <stdio.h>
 
-int main(){
-  printf("Hola, soy 02_04\n");
+void borrpant();
+
+int main()
+{
+  /* versión 4: impresión del código ASCII > */
+  /* del carácter tecleado por el usuario */
+  /* con posibilidad de repetir el proceso */
+
+  char carEjemp, repetir;
+
+  repetir = 'S';
+
+  while (repetir == 'S' || repetir == 's')
+  {
+    borrpant();
+    printf("\nCarácter del que se desea conocer el código ASCII > ");
+    scanf("\n%c", &carEjemp);
+    printf("\nEl código ASCII de %c es %d", carEjemp, carEjemp);
+    repetir = 'x';
+    while (repetir != 'S' && repetir != 's' && repetir != 'N' && repetir != 'n')
+    {
+      printf("\nDesea repetir el proceso (S/N) > ");
+      scanf("\n%c", &repetir);
+    }
+  }
+
+  borrpant();
   return 0;
+}
+
+void borrpant()
+{ /* borra la pantalla */
+  int contador;
+  while (contador != 25)
+  {
+    printf("\n");
+    contador = contador + 1;
+  }
 }
