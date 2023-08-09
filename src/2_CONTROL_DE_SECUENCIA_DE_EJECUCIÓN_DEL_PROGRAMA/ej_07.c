1be2cb6fde6ac7abacc78ce0df4c3a9a403dbb68
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-09 01:25:19 -0300

    resolves ej_07.c

--- src/2_CONTROL_DE_SECUENCIA_DE_EJECUCIÓN_DEL_PROGRAMA/ej_07.c
+++ src/2_CONTROL_DE_SECUENCIA_DE_EJECUCIÓN_DEL_PROGRAMA/ej_07.c
@@ -1,6 +1,46 @@
-#include <stdio.h>
+/***************
+| Ap.02, Ej.06 |
+***************/
+#include <stdio.h>    /* Se puede utilizar en lugar del módulo codcar() del */
+#include <stdlib.h>   /* ej. 6 */
 
-int main(){
-  printf("Hola, soy 02_07\n");
+void borrpant();
+
+int main()
+{
+  int numEjemp;
+  char repetir;
+
+  repetir = 'S';
+  while (repetir == 'S' || repetir == 's')
+  {
+    borrpant();
+    printf("\nCódigo ASCII del que que desea conocer el carácter > ");
+    scanf("\n%d", &numEjemp);
+    if (numEjemp < 33 || numEjemp > 254)
+    {
+      printf("\nEl código debe ser mayor de 33 y menor de 254");
+    }
+    else
+    {
+      printf("\nEl carácter ASCII de %d es %c", numEjemp, numEjemp);
+    }
+    repetir = 'x';
+    while (repetir != 'S' && repetir != 's' && repetir != 'N' && repetir != 'n')
+    {
+      printf("\nDesea repetir el proceso (S/N) > ");
+      scanf("\n%c", &repetir);
+    }
+  }
+
   return 0;
+}
+
+void borrpant()
+{                       /* borra la pantalla */
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
 }
