77f44c3da485af4ea52dab43034a29ede39b17ab
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-10 15:49:19 -0300

    resolves 04 ej_01.c

--- src/4_NUMEROS_Y_TABLAS___LAS_3_EN_RAYA/ej_01.c
+++ src/4_NUMEROS_Y_TABLAS___LAS_3_EN_RAYA/ej_01.c
@@ -1,6 +1,23 @@
+/***************
+| Ap.04, Ej.01 |
+***************/
 #include <stdio.h>
 
+int sumInt(int x,int y);
+
 int main(){
-  printf("Hola, soy 04_01\n");
+  /* programa ejemplo de transferencia de parámetros */
+
+  int numero1, numero2, total;
+  numero1 = 5;
+  numero2 = 3;
+  total = sumInt(numero1, numero2);
+  printf("%d más %d igual a %d", numero1, numero2, total);
+
   return 0;
+}
+
+int sumInt(int x,int y)
+{ /* sumar dos números enteros */
+return x + y;
 }
