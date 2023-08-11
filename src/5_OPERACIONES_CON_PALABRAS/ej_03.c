eb07fcba8db229f27938a6273740d68d78f524a9
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-11 20:43:43 -0300

    resolves 05 ej_03.c

--- src/5_OPERACIONES_CON_PALABRAS/ej_03.c
+++ src/5_OPERACIONES_CON_PALABRAS/ej_03.c
@@ -1,6 +1,24 @@
+/***************
+| Ap.05, Ej.03 |
+***************/
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-  printf("Hola, soy 05_03\n");
+int main()
+{ /* programa ejemplo, uso de un contador en operaciones */
+  /* con una cadena */
+  char nombre[16];
+  int contador;
+
+  contador = 0;
+
+  strcpy(nombre, "Amadeo");
+
+  while(nombre[contador] != '\0')
+  {
+    printf("\nLa letra %d de %s es %c.", contador + 1, nombre, nombre[contador]);
+    contador = contador + 1;
+  }
+
   return 0;
 }
