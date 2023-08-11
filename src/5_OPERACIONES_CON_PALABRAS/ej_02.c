1174edb648bf2e15545c32b6111e546e16e34ee5
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-11 20:43:12 -0300

    resolves 05 ej_02.c

--- src/5_OPERACIONES_CON_PALABRAS/ej_02.c
+++ src/5_OPERACIONES_CON_PALABRAS/ej_02.c
@@ -1,6 +1,15 @@
+/****************
+| Ap.05, Ej.02 |
+****************/
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-  printf("Hola, soy 05_02\n");
+int main(){  /* programa ejemplo, operaciones con una cadena */
+  char nombre[16];
+
+  strcpy(nombre,"Amadeo");
+
+  printf("La segunda letra de %s es %c", nombre, nombre[1]);
+
   return 0;
 }
