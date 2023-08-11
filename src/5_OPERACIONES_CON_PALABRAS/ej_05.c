18993a7ef4f27f4d582b86f9875aa252fd2f579f
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-11 20:44:05 -0300

    resolves 05 ej_05.c

--- src/5_OPERACIONES_CON_PALABRAS/ej_05.c
+++ src/5_OPERACIONES_CON_PALABRAS/ej_05.c
@@ -1,6 +1,24 @@
+/***************
+| Ap.05, Ej.05 |
+***************/
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(){
-  printf("Hola, soy 05_05\n");
+int main()
+{ /* program ejemplo, diálogo con el usuario, versión mejorada */
+  char nombre[21];
+  int contador;
+  printf("\n¿Cómo se llama? > ");
+  scanf("%s", nombre);
+  nombre[0] = toupper(nombre[0]);
+  contador = 1;
+  while (nombre[contador] != '\0')
+  {
+    nombre[contador] = tolower(nombre[contador]);
+    contador = contador + 1;
+  }
+  printf("\nHola, %s. \n", nombre);
+
   return 0;
 }
