9e47d336bbe2ab21225c39df2172cc989f471c75
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-11 20:43:01 -0300

    resolves 05 ej_01.c

--- src/5_OPERACIONES_CON_PALABRAS/ej_01.c
+++ src/5_OPERACIONES_CON_PALABRAS/ej_01.c
@@ -1,6 +1,15 @@
+/****************
+| Ap.05, Ej.01 |
+****************/
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-  printf("Hola, soy 05_01\n");
+int main(){  /* programa ejemplo, iniciliazación de una cadena */
+  char nombre[16];
+
+  strcpy(nombre,"Amadeo");
+
+  printf("%c%c%c",nombre[3], nombre[4], nombre[5]);
+
   return 0;
 }
