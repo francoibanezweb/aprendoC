9e71b09014886435a6908863156d050f4e958cac
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-11 20:43:56 -0300

    resolves 05 ej_04.c

--- src/5_OPERACIONES_CON_PALABRAS/ej_04.c
+++ src/5_OPERACIONES_CON_PALABRAS/ej_04.c
@@ -1,6 +1,15 @@
+/***************
+| Ap.05, Ej.04 |
+***************/
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-  printf("Hola, soy 05_04\n");
+int main(){   /* programa ejemplo, dialogo con el usuario */
+  char nombre[21];
+
+  printf("\n¿Cómo se llama? > ");
+  scanf("%s",nombre);
+  printf("\nHola, %s. \n",nombre);
+
   return 0;
 }
