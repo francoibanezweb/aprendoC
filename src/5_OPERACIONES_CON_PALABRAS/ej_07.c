88d30d8c4c87ee9355663b821f989c0a6b316c61
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-12 18:25:00 -0300

    resolves ej_07.c

--- src/5_OPERACIONES_CON_PALABRAS/ej_07.c
+++ src/5_OPERACIONES_CON_PALABRAS/ej_07.c
@@ -1,6 +1,51 @@
+/***************
+| Ap.05, Ej.07 |
+***************/
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(){
-  printf("Hola, soy 05_07\n");
+void contras();
+
+int main()
+{ /* programa ejemplo uso de contraseña */
+
+  contras();
+
+  printf("\nContraseña correcta, puede utilizar el programa");
+
   return 0;
+}
+
+void contras() /* leer y verificar una contraseña */
+{
+  int contaLet, contaInt;
+  char contras[12];
+
+  contaInt = 0;  /* bucle para dar al usuario tres intentos para hallar la  */
+  while (1 == 1) /* contraseña */
+  {
+    printf("¿Cuál es la contraseña? > ");
+    scanf("%s", contras);  /* pone en mayúsculas la entrada del usuario */
+
+    contaLet = 0;
+    while (contras[contaLet] != '\0')
+    {
+      contras[contaLet] = toupper(contras[contaLet]);
+      contaLet = contaLet + 1;
+    }
+    if (strcmp(contras, "ABRACADABRA") == 0)
+    {
+      break; /* si la contraseña es correcta pasar al programa principal */
+    }
+    else
+    {
+      contaInt = contaInt + 1;
+      if(contaInt == 3)
+      {             /* si se han producido 3 errores al introducir la  */
+        exit(0);     /* contreseña, impedir el acceso al programa */
+      }
+    }
+  }
 }
