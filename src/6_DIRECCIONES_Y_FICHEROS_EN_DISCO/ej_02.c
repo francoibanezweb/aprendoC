b98d94ea8122bf718ddfaecef271eb628aeea3a5
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-13 17:23:31 -0300

    resolves ej_02.c 6

--- src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_02.c
+++ src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_02.c
@@ -1,6 +1,24 @@
+/***************
+| Ap.06, Ej.02 |
+***************/
 #include <stdio.h>
-
+/* programa ejemplo: forma errónea de modificar valores de parámetros */
+int incsum(int x, int y);
 int main(){
-  printf("Hola, soy 06_02\n");
+  int numero1, numero2, total;
+
+  numero1 = 5;
+  numero2 = 3;
+
+  total = incsum(numero1, numero2);
+  printf("\n%d más %d igual a %d.",numero1, numero2,total);
+
   return 0;
+}
+
+int incsum(int x, int y)
+{
+  x = x + 1;
+  y = y + 1;
+  return x + y;
 }
