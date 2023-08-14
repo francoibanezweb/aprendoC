d64118533813bfcc6e854d9ac0011152af2a34d7
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-14 10:20:15 -0300

    resolves ej_05.c 06

--- src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_05.c
+++ src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_05.c
@@ -1,6 +1,31 @@
+/***************
+| Ap.06, Ej.05 |
+***************/
 #include <stdio.h>
 
-int main(){
-  printf("Hola, soy 06_05\n");
+void impresion(int (*tabla)[9]);
+
+int main()
+{ /* impresión de una tabla, método de alto nivel */
+  int contador, tablaEje[9];
+  contador = 0;
+  while (contador < 9) /* asignación de valores a todos los elementos */
+  {                    /* de la tabla */
+    tablaEje[contador] = contador + 1;
+    contador = contador + 1;
+  }
+  impresion(&tablaEje);
   return 0;
+}
+
+void impresion(int (*tabla)[9])
+{
+  int contador;
+
+  contador = 0;
+  while (contador < 9)
+  {
+    printf("\n El elemento %d es igual a %d.", contador, (*tabla)[contador]);
+    contador = contador + 1;
+  }
 }
