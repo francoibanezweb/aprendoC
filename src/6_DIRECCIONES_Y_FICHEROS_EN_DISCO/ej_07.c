849a408ad3f9463dfdfcb062949685d906766205
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-14 10:44:52 -0300

    resolves ej_07.c 06

--- src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_07.c
+++ src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_07.c
@@ -1,6 +1,30 @@
+/***************
+| Ap.06, Ej.07 |
+***************/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  printf("Hola, soy 06_07\n");
+int main()
+{ /* añadir los datos introducidos por el usuario a un fichero  */
+  /* de texto en disco */
+  FILE *puntFich;
+  char datosUsu;
+
+  puntFich = fopen("notas.txt", "a");
+
+  if (puntFich == NULL)
+  {
+    printf("Error de apertura de fichero");
+    exit(0);
+  }
+  printf("\nIntroduzca el texto que desee añadir al fichero > ");
+
+  while (datosUsu != '\n')
+  {
+    scanf("%c", &datosUsu);
+    putc(datosUsu, puntFich);
+  }
+  fclose(puntFich);
+
   return 0;
 }
