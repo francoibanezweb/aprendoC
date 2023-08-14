dc354fb58428aa21d86985b49147d97e8fe7291c
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-14 10:20:25 -0300

    resolves ej_06.c 06

--- src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_06.c
+++ src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_06.c
@@ -1,6 +1,20 @@
+/***************
+| Ap.06, Ej.06 |
+***************/
 #include <stdio.h>
 
-int main(){
-  printf("Hola, soy 06_06\n");
+int main(){ /* escribir los datos introducidos por el usuario en un fichero  */
+  FILE *puntFich; /* de texto en disco */
+  char datosUsu;
+
+  puntFich = fopen("notas.txt", "w");
+  printf("\nIntroduzca el texto que desee archivar > ");
+  while(datosUsu != '\n')
+  {
+    scanf("%c",&datosUsu);
+    putc(datosUsu,puntFich);
+  }
+  fclose(puntFich);
+
   return 0;
 }
