a4be272a7ca41267e5ded1a9bd93da3bc9193336
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-23 12:05:01 -0300

    resolves ej_08.c ap 6

--- src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_08.c
+++ src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_08.c
@@ -1,6 +1,25 @@
+/***************
+| Ap.06, Ej.08 |
+***************/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  printf("Hola, soy 06_08\n");
+int main()
+{ /* leer el fichero de notas */
+  FILE *puntFich;
+  char escritur;
+  puntFich = fopen("notas.txt", "r");
+  if (puntFich == NULL)
+  {
+    printf("Error de apertura de fichero");
+    exit(0);
+  }
+  while (escritur != EOF)
+  {
+    escritur = getc(puntFich);
+    printf("%c", escritur);
+  }
+  fclose(puntFich);
+
   return 0;
 }
