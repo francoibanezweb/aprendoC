a3a585a83dc3b64c192288d4b61f2abba7cb708c
francoibanezweb <francoibanez.dev@gmail.com>
2023-08-28 09:47:32 -0300

    resolves ej_11.c ap 6

--- src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_11.c
+++ src/6_DIRECCIONES_Y_FICHEROS_EN_DISCO/ej_11.c
@@ -1,6 +1,63 @@
+/***************
+| Ap.06, Ej.11 |
+***************/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  printf("Hola, soy 06_11\n");
+#define MAXPALAB 100
+
+struct LisPalEA
+{
+  char Espanol[16];
+  char Aleman[16];
+  char elemento;
+} palabra[MAXPALAB];
+
+int main()
+{
+  /* leer lista de palabras método de bajo nivel */
+
+  FILE *puntFich;
+
+  char letra;
+  char *actual;
+  int contaLet = 0;
+  int contaPru = 0;
+
+  if ((puntFich = fopen("listapala.txt", "x")) == NULL)
+  {
+    printf("Error de Disco: no puede abrirse ");
+    printf("fichero de palabras");
+    exit(0);
+  }
+
+  actual = &(palabra[0].Espanol[0]);
+
+  while (letra != EOF)
+  {
+    letra = getc(puntFich);
+    *actual = letra;
+    actual = actual + 1;
+    contaLet = contaLet + 1;
+
+    if (contaLet / 33 > MAXPALAB)
+    {
+      printf("\nERROR: lista de palabras, demasiado larga.");
+      printf("\n\tEl programa sólo utilizará, %d palabras.", MAXPALAB);
+      printf("\n\tSi desea utilizar todas las palabras");
+      printf("\n\tpreviamente el programa.");
+      break;
+    }
+  }
+
+  fclose(puntFich);
+  while (contaPru < (contaLet / 33))
+  {
+    printf("\n\n%s", palabra[contaPru].Espanol);
+    printf("\n%s", palabra[contaPru].Aleman);
+    printf("\n%c", palabra[contaPru].elemento);
+    contaPru = contaPru + 1;
+  }
+
   return 0;
 }
